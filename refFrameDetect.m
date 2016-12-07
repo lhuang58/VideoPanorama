a027920dbb4fb69e7979b43c5e9640ffa14ad762
lhuang58 <nick>
2016-12-07 16:18:48 -0600

    Reference Frame Detect tentative

--- refFrameDetect.m
+++ refFrameDetect.m
@@ -1,11 +1,31 @@
 function referenceFrame = refFrameDetect(S)
 % This function will return a reference from a given video segment
+    
+    frameDistances = cell(1, length(S));
+    [H_left, H_right] = generateH_lists(S);
+    
+    for i = 1 : length(S)
+        % For all the frames in given segment find the reference frame with
+        % minimum coverage
+        % For example [f1, f2, f3, f4, ... fn], if f1 = fr, then align all
+        % other frame fi to f1. And calculate all the frames in the segment
+        % choose the one whose panorama is smallest
+        H_map = calMapping(H_left, H_right, i);
+        frameDistances{i} = centerDistance(H_map, S);  
+    end
+    % Find the smallest distance and return the index accordingly.
+    [minValue, index] = min(cell2mat(frameDistances));
+    referenceFrame = index;
+end
+
+function [H_list_left, H_list_right] = generateH_lists(S)
     for i = 1 : length(S)
         input_image = single(rgb2gray(S{i}));
         [keypoints{i}, descriptors{i}] = vl_sift(input_image);
     end
     % Preallocate for performance
-    H_list = cell(1, length(S) - 1);
+    H_list_left = cell(1, length(S) - 1);
+    H_list_right = cell(1, length(S) - 1);
     
     for i=1:length(S)-1
 
@@ -26,56 +46,27 @@ function referenceFrame = refFrameDetect(S)
         % into coordinates in image1. Function calcHWithRANSAC currently uses all
         % pairs of matching feature points returned by the SIFT algorithm.
         % Modify the calcHWithRANSAC function to add code for implementing RANSAC.
-        H_list{i} = calcHWithRANSAC(im1_ftr_pts, im2_ftr_pts);
-    end
-    
-    frameDistances = cell(1, length(S));
-    for i = 1 : length(S)
-        % For all the frames in given segment find the reference frame with
-        % minimum coverage
-        % For example [f1, f2, f3, f4, ... fn], if f1 = fr, then align all
-        % other frame fi to f1. And calculate all the frames in the segment
-        % choose the one whose panorama is smallest
-        H_map = calHomography(H_list, i);
-        frameDistances{i} = centerDistance(H_map, S);  
-    end
-    % Find the smallest distance and return the index accordingly.
-    referenceFrame = 0;
-    minDistance = inf;
-    for i = 1 : length(frameDistances)
-        curDistance = frameDistances;
-        if frameDistances{i} < curDistance
-            minDistance = frameDistances{i};
-            referenceFrame = i;
-        end
+        H_list_left{i} = calcHWithRANSAC(im1_ftr_pts, im2_ftr_pts);
+        H_list_right{i} = calcHWithRANSAC(im2_ftr_pts, im1_ftr_pts);
     end
 end
 
-function H_map = calHomography(H_list, referenceIndex)
+
+function H_map = calMapping(H_list_left, H_list_right, referenceIndex)
 % This function will return a list of homography 
     % Calculate homography according to reference frame index
-    H_map = {};
+    H_map = cell(1, length(H_list_left) + 1);
     % The homography for the reference frame is identity matrix
-    H_map{referenceIndex} = eye(size(H_list{1}));
-    
-    % Recalculate the homography for frames' index before the reference
-    % frame
-    
+    H_map{referenceIndex} = eye(size(H_list_left{1}));
     % If reference frame is at index i, then we calculate the homography
     % for frames from 1 to i by Hi-1i*Hi-2i-1*...*H23H12 where Hij is the
     % homography mapping i to j
-    if referenceIndex > 1
-        for i = referenceIndex : -1 : 2
-            base = H_map{i};
-            base = H_list{i - 1} * base;
-            H_map{i - 1} = base;
-        end
+    for i = referenceIndex - 1: -1 : 1
+        H_map{i} = H_list_left{i} * H_map{i + 1};
     end
     
-    for i = referenceIndex : length(H_list)
-        base = H_map{i};
-        base = H_list{i} * base;
-        H_map{i + 1} = base;
+    for i = referenceIndex + 1: length(H_map)
+        H_map{i} = H_list_right{i - 1} * H_map{i - 1};
     end
 end
 
